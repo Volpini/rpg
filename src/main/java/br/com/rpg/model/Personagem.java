d7a402e83986f8a764e9ce3f1b3bd62fd4ce44da
EBAndroid <erickguitar_man@hotmail.com>
2015-02-12 12:36:26 -0300

    Cadastro de Habilidades

--- src/main/java/br/com/rpg/model/Personagem.java
+++ src/main/java/br/com/rpg/model/Personagem.java
@@ -3,6 +3,7 @@ package br.com.rpg.model;
 import br.com.rpg.component.grid.GridColumn;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +13,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -33,12 +35,24 @@ public class Personagem implements Serializable {
 	private SystemUser systemUser;
 	@ManyToMany
 	private List<Talento> talentos;
-	@NotNull
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "personagem")
+
+	@OneToMany(mappedBy = "personagem")
 	@OnDelete(action = OnDeleteAction.CASCADE)
+        @Cascade(org.hibernate.annotations.CascadeType.ALL)
 	private List<PersonagemItem> itensPersonagem;
 	
-	public Long getId() {
+        @OneToMany(mappedBy = "personagem")
+        @OnDelete(action = OnDeleteAction.CASCADE)
+        @Cascade(org.hibernate.annotations.CascadeType.ALL)
+        private List<PersonagemHabilidade> habilidades;
+        
+        @OneToMany(cascade = CascadeType.ALL, mappedBy = "personagem")
+        @OnDelete(action = OnDeleteAction.CASCADE)
+        @Cascade(org.hibernate.annotations.CascadeType.ALL)
+        private List<PersonagemPericia> pericias;
+        
+        
+      	public Long getId() {
 		return id;
 	}
 
@@ -81,4 +95,32 @@ public class Personagem implements Serializable {
 	public String getForSelect(){
 		return name;
 	}
+
+    /**
+     * @return the habilidades
+     */
+    public List<PersonagemHabilidade> getHabilidades() {
+        return habilidades;
+    }
+
+    /**
+     * @return the pericias
+     */
+    public List<PersonagemPericia> getPericias() {
+        return pericias;
+    }
+
+    /**
+     * @param habilidades the habilidades to set
+     */
+    public void setHabilidades(List<PersonagemHabilidade> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    /**
+     * @param pericias the pericias to set
+     */
+    public void setPericias(List<PersonagemPericia> pericias) {
+        this.pericias = pericias;
+    }
 }
