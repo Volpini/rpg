c5258adf95f4822f99aa44f98a23fd3cd16e32d7
Volpini <bruno-volpini@hotmail.com>
2015-02-13 15:32:50 -0300

    Removida importacao nao utilizada

--- src/main/java/br/com/rpg/model/PersonagemHabilidade.java
+++ src/main/java/br/com/rpg/model/PersonagemHabilidade.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.com.rpg.model;
 
 import java.io.Serializable;
@@ -11,83 +5,80 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import org.hibernate.annotations.Cascade;
 
 /**
  *
  * @author ECLima
  */
 @Entity
-public class PersonagemHabilidade implements Serializable{
-    
-    @Id
-    @GeneratedValue
-    private Long id;
-    
-    
-    @ManyToOne
-    private Habilidade habilidade;
-    
-    @ManyToOne
-    private Personagem personagem;
-    
-    private Integer valor;
+public class PersonagemHabilidade implements Serializable {
 
-    /**
-     * @return the id
-     */
-    public Long getId() {
-        return id;
-    }
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@ManyToOne
+	private Habilidade habilidade;
 
-    /**
-     * @return the habilidade
-     */
-    public Habilidade getHabilidade() {
-        return habilidade;
-    }
+	@ManyToOne
+	private Personagem personagem;
 
-    /**
-     * @param habilidade the habilidade to set
-     */
-    public void setHabilidade(Habilidade habilidade) {
-        this.habilidade = habilidade;
-    }
+	private Integer valor;
 
-    /**
-     * @return the valor
-     */
-    public Integer getValor() {
-        return valor;
-    }
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
 
-    /**
-     * @param valor the valor to set
-     */
-    public void setValor(Integer valor) {
-        this.valor = valor;
-    }
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    /**
-     * @return the personagem
-     */
-    public Personagem getPersonagem() {
-        return personagem;
-    }
+	/**
+	 * @return the habilidade
+	 */
+	public Habilidade getHabilidade() {
+		return habilidade;
+	}
 
-    /**
-     * @param personagem the personagem to set
-     */
-    public void setPersonagem(Personagem personagem) {
-        this.personagem = personagem;
-    }
-    
-    
+	/**
+	 * @param habilidade the habilidade to set
+	 */
+	public void setHabilidade(Habilidade habilidade) {
+		this.habilidade = habilidade;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public Integer getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(Integer valor) {
+		this.valor = valor;
+	}
+
+	/**
+	 * @return the personagem
+	 */
+	public Personagem getPersonagem() {
+		return personagem;
+	}
+
+	/**
+	 * @param personagem the personagem to set
+	 */
+	public void setPersonagem(Personagem personagem) {
+		this.personagem = personagem;
+	}
+
 }
